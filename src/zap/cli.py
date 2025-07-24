8777f216a6081759c62758a3bfd7de1c576bcd91
jesusjsg <jesusjsgdev@gmail.com>
2025-07-24 02:07:32 +0000

    feat: example in help option

--- src/zap/cli.py
+++ src/zap/cli.py
@@ -1,5 +1,4 @@
 import click
-import os
 from pathlib import Path
 
 
@@ -9,10 +8,24 @@ def cli():
     pass
 
 
-@click.group()
-def files():
-    """Files commands"""
-    pass
+@cli.command()
+@click.option(
+    "-e", "--ext", type=click.STRING, help="File extension to search for (e.g .py)"
+)
+@click.argument("dir", type=click.Path(exists=True, path_type=Path))
+def list(dir: Path, ext: str | None) -> None:
+    """List current directory files"""
+    if not dir.is_dir():
+        click.echo(f"{dir} is not a directory")
+        raise SystemExit(1)
+
+    for file in dir.iterdir():
+        if file.is_file():
+            if ext:
+                if file.suffix.lower() == ext.lower():
+                    click.secho(
+                        f"{file.name:{len(file.name) + 5}}", nl=False, fg="green"
+                    )
 
 
 @cli.command()
