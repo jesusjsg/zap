447a658052bc2100230a0473de247b5eba95431f
jesusjsg <jesusjsgdev@gmail.com>
2025-07-25 03:25:00 +0000

    feat: list files using zl get the long and extension file info

--- src/zap/cli.py
+++ src/zap/cli.py
@@ -1,5 +1,28 @@
 import click
 from pathlib import Path
+from datetime import datetime
+
+
+def _validate_extension(file: Path, ext: str | None) -> bool:
+    if ext is None:
+        return True
+    return file.suffix.lower() == ext.lower()
+
+
+def _get_file_info(file: Path) -> tuple[int, str, str]:
+    size = file.stat().st_size
+    date = datetime.fromtimestamp(file.stat().st_mtime).strftime("%Y-%m-%d %H:%M:%S")
+    name = file.name
+    return size, date, name
+
+
+def _print_output(file: Path, long: bool = False) -> None:
+    if long:
+        size, date, name = _get_file_info(file)
+        click.secho(f"{size}\t {date}\t", nl=False)
+    name = f"{file.name}" if file.is_dir() else file.name
+    color = "blue" if file.is_dir() else "green"
+    click.secho(name, fg=color)
 
 
 @click.group()
@@ -9,29 +32,39 @@ def cli():
 
 
 @cli.command()
-@click.option(
-    "-e", "--ext", type=click.STRING, help="File extension to search for (e.g .py)"
-)
-@click.argument("dir", type=click.Path(exists=True, path_type=Path))
-def list(dir: Path, ext: str | None) -> None:
+@click.option("-e", "--ext", type=click.STRING, help="Listing files with extension")
+@click.option("-l", "--long", is_flag=True, help="Long listing")
+@click.argument("dir", type=click.Path(exists=True, path_type=Path), default=".")
+def zl(dir: Path, ext: str | None, long: bool) -> None:
     """List current directory files"""
     if not dir.is_dir():
         click.echo(f"{dir} is not a directory")
         raise SystemExit(1)
 
     for file in dir.iterdir():
-        if file.is_file():
-            if ext:
-                if file.suffix.lower() == ext.lower():
-                    click.secho(
-                        f"{file.name:{len(file.name) + 5}}", nl=False, fg="green"
-                    )
+        if file.is_dir() and ext:
+            continue
+        if file.is_file() and not _validate_extension(file, ext):
+            continue
+        _print_output(file, long)
+
+
+@cli.command()
+@click.option(
+    "-e",
+    "--ext",
+    type=click.STRING,
+    help="File extension to zap the files (e.g .py)",
+)
+@click.argument("path", type=click.Path(exists=True, path_type=Path))
+def zd(path: Path, ext: str | None) -> None:
+    """Zap current directory files"""
 
 
 @cli.command()
 def version():
     """Version of zap"""
-    click.echo("zap version 0.0.1")
+    click.secho("zap version 0.0.1", fg="green")
 
 
 if __name__ == "__main__":
